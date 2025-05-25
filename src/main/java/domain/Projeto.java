fe38ccbaa46ea9779618d06df1ce892a4c92b36f
Luiz Fellipe <54127176+luizfellipelb@users.noreply.github.com>
2025-05-24 21:43:55 -0300

    adicionado public class Projeto -> package domain

--- src/main/java/domain/Projeto.java
+++ src/main/java/domain/Projeto.java
@@ -1,7 +1,38 @@
 package domain;
 
 public class Projeto {
-    private int id_projeto;
+    private Long id_projeto;
     private String nome;
     private String descricao;
+
+    public Projeto() { }
+
+    public Projeto(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id_projeto;
+    }
+
+    public void setId(Long id) {
+        this.id_projeto = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
